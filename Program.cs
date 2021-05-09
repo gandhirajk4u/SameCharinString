7f1d9a16576b8f75fb7c21de70d8a2bfbd8247f7
Gandhiraj Kandasamy <gandhirajk4u@hotmail.com>
2021-05-09 19:31:31 +0530

    Latest changes

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SameCharinString
+{    public static class Program
+    {
+        private static string GetChar(string str)
+        {
+            int i, j;
+            string strOutput = "";
+            for (i = 0; i < str.Length; i++)
+            {
+                for (j = i + 1; j < str.Length; j++)
+                {
+                    if(str[i] == str[j])
+                    {
+                        strOutput += str[i].ToString() + "\n";
+                    }
+                }
+            }
+            return strOutput;
+        }
+        static void Main(string[] args)
+        {
+            string op = GetChar("Vainav-Vennila");
+            Console.WriteLine(op);
+            Console.ReadLine();
+        }
+    }
+}
